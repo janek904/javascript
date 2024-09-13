9d1f69379b1d6ab7383248ae71a81c7949874d26
janek904 <jn533424@gmail.com>
2024-09-13 12:45:21 +0200

    xvxvx
    vxvxvx

--- kolumny/kolumny.js
+++ kolumny/kolumny.js
@@ -9,6 +9,38 @@ myheader.textContent = 'Preambuła';
  myset[0].style.border = '2px solid brown';
  myset[1].style.border = '2px solid brown';
  myset[2].style.border = '2px solid brown';
+ document.querySelector('.main_column h1').textContent = 'Specjalność szefa:';
+
+const kol = document.querySelector('.main_column');
+const para = document.createElement('p');
+para.textContent = 'Kebab na frytkach _-_';
+kol.appendChild(para);
+const footer = document.querySelector('footer');
+const div = document.createElement('div');
+div.style.backgroundColor = 'pink';
+div.style.padding ='10px';
+div.style.textAlign ='center';
+const xyz = document.createElement('p');
+xyz.innerHTML = '&copy';
+footer.appendChild(div);
+div.appendChild(xyz);
+
+const kolumna = document.querySelector('#eee h1');
+kolumna.remove();
+const head = document.querySelector('header');
+const pgf= document.createElement('p');
+const pgff = document.createElement('p');
+pgf.innerHTML = '<i> Pochylony tekst </i>';
+pgff.innerText = '<i> Pochylony tekst </i>';
+head.appendChild(pgf);
+head.appendChild(pgff);
+const bod = document.querySelector('body');
+const sec = document.createElement('section');
+sec.style.border = '2px dotted blue';
+sec.style.padding = ' 10px 20px'; 
+sec.textContent = 'zderzak';
+bod.appendChild(sec);
+
 
 
 
