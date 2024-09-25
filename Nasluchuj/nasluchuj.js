0f2340c55976f78cedb34f40e1e59c34922c9cd6
janek904 <jn533424@gmail.com>
2024-09-25 13:51:35 +0200

    xgfxgx
    xvvxvxv

---
+++ Nasluchuj/nasluchuj.js
@@ -0,0 +1,5 @@
+const przyciski = document.querySelector('#guzik-h6')
+const element1 = document.getElementById('hello')
+przyciski.addEventListener("click", function(){
+    element1.innerHTML="<i>Świetnie</i>";
+}) 
